6436feb46b14f9cb4c68887011ce4c4b3eab81e0
Abel Calderon <rudy_gamerser@hotmail.com>
2020-12-11 10:49:52 -0500

    Agregado compra

---
+++ Controlador/compra.php
@@ -0,0 +1,19 @@
+<?php 
+    class Compra extends Controladores{
+        function __construct(){
+            parent::__construct();
+        }
+        public function insertar_compra(){
+            $data = $this->modelo->modelo_inserta_compra("12345678",null,null,null);
+            $this->vistas->obten_vista($this,"ver_compra",$data);
+        }
+        public function listar_compra(){
+            $data = $this->modelo->modelo_listar_compra();
+            $this->vistas->obten_vista($this,"ver_compra",$data);
+        }
+        public function busca_compra(){
+            $data = $this->modelo->modelo_busca_compra(1);
+            $this->vistas->obten_vista($this,"ver_compra",$data);
+        }
+    }
+?>
---
+++ Modelos/compra_modelo.php
@@ -0,0 +1,43 @@
+<?php
+    class compra_modelo extends sql_server{
+        public function __construct(){
+            parent::__construct();
+        }
+        public function modelo_inserta_compra(
+            $ruc_dni,
+            $serie_factura_boleta,
+            $correlativo_factura_boleta,
+            $fecha_compra_realizada
+        ){
+            //FALTA VALIDAR CUANDO ME ENVIEN UNA COMPRA CON FECHA DE COMPRA REALIZADA
+            $fecha_registro_compra = date("Y-m-d");
+            $query = "INSERT INTO compra(
+                ruc_dni,
+                serie_factura_boleta,
+                correlativo_factura_boleta,
+                fecha_registro_compra,
+                fecha_compra_realizada
+                ) 
+                values (?,?,?,?,?)";
+            $valores = array($ruc_dni,$serie_factura_boleta,$correlativo_factura_boleta,$fecha_registro_compra,$fecha_compra_realizada);
+            $solicita_insert = $this->insert($query,$valores);
+            return $solicita_insert;
+        }
+        public function modelo_listar_compra(){
+            $query = "SELECT * FROM compra";
+            $solicita_listado = $this->select_all($query);
+            return $solicita_listado;
+        }
+        public function modelo_busca_compra($id_compra){
+            $query = "SELECT * FROM  compra WHERE id_compra = $id_compra";
+            $solicita_busqueda = $this->select_one($query);
+            return $solicita_busqueda;
+        }
+        //Directamente no se debe aplicar un delete from xd 
+        /*public function modelo_elimina_cliente($id_venta){
+            $query = "DELETE FROM venta WHERE id_venta = '$id_venta'";
+            $solicita_borrado = $this->delete($query);
+            return $solicita_borrado;
+        }*/
+    }
+?>
