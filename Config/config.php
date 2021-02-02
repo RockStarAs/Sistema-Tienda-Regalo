5c7445fcf336a292ffcd5a567258d52fda7c68ee
Abel Calderon <rudy_gamerser@hotmail.com>
2021-02-02 17:29:44 -0500

    make it better

--- Config/config.php
+++ Config/config.php
@@ -6,18 +6,18 @@
     const METHOD = 'aes-256-cbc';
     date_default_timezone_set('America/Lima');
     //PARA SQL SERVER
-    /* 
+     
     const DB_USER = "abel";
     const DB_PASSWORD = "cien100%";
     const DB_NAME = "bd_tienda";
     const DB_HOST = "servbd.database.windows.net";
-    */
+    
     //PARA MYSQL
-    const DB_USER = "root";
+    /*const DB_USER = "root";
     const DB_PASSWORD = "";
     const DB_NAME = "datos_tienda";
     const DB_HOST = "localhost";
-
+*/
     const SPD=".";
     const SPM=",";
     const VERSION_BD = "MySQL";
